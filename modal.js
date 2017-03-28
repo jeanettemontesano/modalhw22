7b818e9007ab8b5a03ca2f6a6391e438f0e5bfe2
jeanette montesano <escapetheearth@gmail.com>
2017-03-28 16:27:12 -0400

    modal222

--- modal.js
+++ modal.js
@@ -1,1 +1,17 @@
-setTimeout(function(){ alert("Welcome to Acme"); }, 3000);
+var modalOpen = document.getElementById('modal');
+
+modalOpen.style.display = "none";
+
+setTimeout(delay, 3000);
+
+function delay() {
+  modalOpen.style.display = "block";
+};
+
+
+// close modal
+var closeBtn = document.getElementById('closeBtn');
+
+closeBtn.onclick = function() {
+  modalOpen.style.display = "none";
+}
